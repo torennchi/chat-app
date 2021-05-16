01d238493f72a67215e26149ef2c0bffb0183f0a
torennchi <xiangximachang@gmail.com>
2021-05-16 16:58:44 +0900

    room_usersテーブルの作成

--- db/migrate/20210516061736_create_rooms.rb
+++ db/migrate/20210516061736_create_rooms.rb
@@ -1,7 +1,8 @@
 class CreateRooms < ActiveRecord::Migration[6.0]
   def change
     create_table :rooms do |t|
-      t.string :name, null: false
+      t.refernces :room, foreign_key: true
+      t.refernces :user, foreign_key: true
       t.timestamps
     end
   end
