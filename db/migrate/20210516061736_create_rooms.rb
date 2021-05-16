8b5a6f7417de7e9bd36c4b13641a7cf387bb97ae
torennchi <83404526+torennchi@users.noreply.github.com>
2021-05-16 19:01:31 +0900

    Revert "チャットルーム管理機能の実装"

--- db/migrate/20210516061736_create_rooms.rb
+++
@@ -1,9 +0,0 @@
-class CreateRooms < ActiveRecord::Migration[6.0]
-  def change
-    create_table :rooms do |t|
-      t.refernces :room, foreign_key: true
-      t.refernces :user, foreign_key: true
-      t.timestamps
-    end
-  end
-end
